aa93b7929896aefd146c5b2ff80162217e4c2a6b
cassi <cassi@local>
2026-03-07 16:24:42 -0500

    changed the IP address to the emulator

--- www/js/services_courses.js
+++ www/js/services_courses.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('CITC.services', ['ngResource'])
-.constant("baseURL","http://localhost:3000/")
+// .constant("baseURL","http://localhost:3000/")
+.constant("baseURL","http://10.0.2.2:3000/")
 //.constant("baseURL","http://169.254.234.127:3000/")
 //.constant("baseURL","http://192.168.0.76:3000/")
 // .constant("baseURL","http://10.0.1.13:3000/")
